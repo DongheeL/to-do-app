fc44092fef6de052d2940cb031c86ae8ab95431e
DongheeL <donghlee19@gmail.com>
2023-03-24 20:35:31 +0900

    스타일링

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import styles from './AddTodo.module.css'
 
 export default function AddTodo({onAdd}) {
     const [input, setInput] = useState('');
@@ -16,9 +17,11 @@ export default function AddTodo({onAdd}) {
         setInput('');
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <input placeholder="Add Todo" value={input} onChange={handleChange}></input>
-            <button>Add</button>
+        <form className={styles.form} onSubmit={handleSubmit}>
+            <input 
+                className={styles.input}
+                placeholder="Add Todo" value={input} onChange={handleChange}/>
+            <button className={styles.button}>Add</button>
         </form>
     );
 }
